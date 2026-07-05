bff6a4803d1565040ff3e8e39e31bddc655adf16
absol255 <mewoisthebest123456789@gmail.com>
2026-07-05 15:38:14 -0700

    AAAAAAAAAAAAAAA NEW THIG

---
+++ static/js/otherminiwebsites.js
@@ -0,0 +1,32 @@
+(function () {
+    function gotoStockMarket() {
+        window.location.href = "https://macho-stock-market.vercel.app";
+    }
+
+    function gotoShop() {
+        window.location.href = "https://macho-shop.vercel.app";
+    }
+
+    function gotoOldWebsite() {
+        window.location.href = "https://macho4life.store";
+    }
+
+    function gotoBank() {
+        window.location.href = "https://macho-buck-webapp.vercel.app";
+    }
+
+    function gotoStarTesting() {
+        window.location.href = "https://macho-star-testing.vercel.app"
+    }
+
+    function gotoEBay() {
+        window.location.href = "https://macho-e-bay.vercel.app"
+    }
+
+    function gotoRankings() {
+        window.location.href = "https://macho-rankings.vercel.app"
+    }
+
+    
+
+})();
